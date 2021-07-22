e03452a995173e6d7a5e1fd9a8aef252a81e5099
Dondre <74502839+Drw2366@users.noreply.github.com>
2021-07-21 20:39:30 -0400

    just added a button to the feature

--- horns-animals/src/components/HornedBeast.js
+++ horns-animals/src/components/HornedBeast.js
@@ -1,21 +1,30 @@
 import React from 'react';
+// import black from '..//img/hb2.jpg';
+// another way to import could also use import/from
+//useState: allows us to just render one peice of code 
+const {useState} = React;
 
 
 
 function HornedBeast({image_url, title, description}) {
+  //function to give the button a job
+  const handleClick = () =>{
+    setCount(count+1);
+  };
+  //userState(sets beg value)
+  const [count, setCount] = useState(100); 
+
   return (
     <>
-      <h2>
-        {title}
-      </h2>
-
+      <h2>{title}</h2>
       <img height="500px" src={image_url} alt="Bird Picture" title="Bird Pic"/>;
+      <p>{description}</p>
 
-      <p>
-        {description}
-      </p>
 
+      <button onClick={handleClick}> Favorite {count} </button>
     </>
+
+    
       
       
       
