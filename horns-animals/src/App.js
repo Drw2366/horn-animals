0b2e00f3e1d9345dd722fa151958dcc605a83945
Drw2366 <74502839+Drw2366@users.noreply.github.com>
2021-08-27 17:28:38 -0400

    Finished up lab03

--- horns-animals/src/App.js
+++ horns-animals/src/App.js
@@ -1,42 +1,26 @@
 // import logo from './logo.svg';
 import './App.css';
-import Footer from './components/Footer';
 import Header from './components/Header';
+import Footer from './components/Footer';
 import Main from './components/Main';
-import {animalData} from './data.js';
-
-
+import { animalData } from './data';
+// import { useState } from 'react';
 
 
 function App() {
+  // const [modalOpen, setModalOpen] = useState(false);
+  // let toggleModal  = () => {
+  //   setModalOpen(prevModalOpen => !prevModalOpen);
+  // };
   return (
     <div className="App">
       <Header/>
-      <Main animalData = {animalData}/>
+      <Main 
+        animalData={animalData}
+      />
       <Footer/>
-      
-      
-      {/* <header className="App-header">
-         */}
-      {/* <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.???
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
     </div>
   );
-
-
 }
-
-
 
 export default App;
--- horns-animals/src/components/Main.js
+++ horns-animals/src/components/Main.js
@@ -1,44 +1,23 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Row, Col } from 'react-bootstrap';
 import HornedBeast from './HornedBeast';
-import {useState} from 'react';
-import {Container, Row, Col, Dropdown} from 'react-bootstrap';
+// import SelectedBeast from './SelectedBeast';
 
 function Main({animalData}) {
-  let filterArray = [1, 2, 3, 100];
-  const [selectedHorns, setSelectedHorns] = useState(0);
   return (
     <>
       <Container>
         <Row>
-              
-          <Col xs={12}>
-            <Dropdown>
-              <Dropdown.Toggle variant="success" id="Filter Dropdown">
-                Filter Beast by Horn Amount
-              </Dropdown.Toggle>
-
-              <Dropdown.Menu>
-                {filterArray.map(number => (
-                  <Dropdown.Item
-                    onSelect={(eventKey)=> setSelectedHorns(parseInt(eventKey))}
-                    eventKey={number}>
-                    {number} Horn(s)
-                  </Dropdown.Item>
-                ))}
-              </Dropdown.Menu>
-            </Dropdown>
-          </Col>
-
-          {animalData.filter(data=> data.horns === selectedHorns
-          ).map((filteredData, key) => {
+          {animalData.map((data, key) => {
             return (
               <Col xs={12} md={6} lg={4}>
                 <div key={key}>
                   <HornedBeast
                     key={key}
-                    image_url={filteredData.image_url}
-                    title={filteredData.title}
-                    description={filteredData.description}
+                    image_url={data.image_url}
+                    title={data.title}
+                    description={data.description}
                   />
                 </div>
               </Col>
@@ -51,30 +30,3 @@ function Main({animalData}) {
 }
 
 export default Main;
-
-
-
-
-
-
-
-
-// function Main() {
-//   return (
-//     <>
-//       <HornedBeast 
-//         title="Peacock" 
-//         imgurl={Peacock} 
-//         desc="A flashy type of bird. A white peacock is very rare and very hard to find."
-//       />
-//       <HornedBeast 
-//         title="Car" 
-//         imgurl={Car} 
-//         desc="One of the worlds fastest cars."
-//       />
-//     </>
-//   );
-
-// }
-// export default Main;
-
